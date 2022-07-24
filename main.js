40431924a42db189c586ec00ea1644a07fe1abc8
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-24 18:32:53 +0430

    js file was updated and addTodo function was added again

--- main.js
+++ main.js
@@ -1,38 +1,24 @@
 // get element from DOM
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
-const plusIcon = document.querySelector(".fa-circle-plus");
+const addBtn = document.querySelector(".btn");
 const todosContainer = document.querySelector(".todos");
 
-// add todos function
-const addTodo = function () {
+const addTodo = function (val) {
   const html = `
-      <div class="todo">
-           <li class="todo-item">${input.value}</li>
-           <div class="icons">
-             <i class="fa-solid fa-square-check"></i>
-             <i class="fa-solid fa-trash-can"></i>
-           </div>
-       </div>
-      `;
+    <div class="todo">
+         <li class="todo-item">${val}</li>
+         <div class="icons">
+           <i class="fa-solid fa-square-check"></i>
+           <i class="fa-solid fa-trash-can"></i>
+         </div>
+     </div>
+    `;
   todosContainer.insertAdjacentHTML("afterbegin", html);
-  input.value = "";
 };
 
-todosContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("fa-trash-can")) {
-    e.target.parentElement.parentElement.remove();
-  } else if (e.target.classList.contains("fa-square-check")) {
-    e.target.parentElement.parentElement.style.opacity = ".5";
-    e.target.parentElement.previousElementSibling.style.textDecoration =
-      "line-through";
-  }
-});
-
-form.addEventListener("submit", (e) => {
+addBtn.addEventListener("submit", (e) => {
   e.preventDefault();
-  addTodo();
-});
-plusIcon.addEventListener("click", () => {
-  addTodo();
+  addTodo(input.value);
+  input.value = "";
 });
