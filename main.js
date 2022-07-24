336c998f735f554d8427378c444d3f8dacdc4fd2
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-24 18:02:32 +0430

    get elements from DOM

---
+++ main.js
@@ -0,0 +1,5 @@
+// get element from DOM
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const plusIcon = document.querySelector(".fa-circle-plus");
+const todosContainer = document.querySelector(".todos");
